73361e99bab3e4d83e8854c2d0a38a407110c1a0
thuan <ducthuan051291@gmail.com>
2022-10-10 10:29:03 +0700

    ok

---
+++ src/main/java/com/daicent1/demojpa/daicent1/bean/SchoolSevicer.java
@@ -0,0 +1,34 @@
+package com.daicent1.demojpa.daicent1.bean;
+
+import com.daicent1.demojpa.daicent1.model.School;
+import com.daicent1.demojpa.daicent1.model.Student;
+import com.daicent1.demojpa.daicent1.repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class SchoolSevicer {
+    @Autowired
+    SchoolRepository schoolRepository;
+    public List<School>findAll(){
+      return   schoolRepository.findAll();
+    }
+
+    public Optional<School> findById(Long id) {
+        return schoolRepository.findById(id);
+    }
+
+
+    public void save(School school) {
+        schoolRepository.save(school);
+
+    }
+
+
+    public void remove(Long id) {
+        schoolRepository.deleteById(id);
+    }
+}
